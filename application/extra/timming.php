ddca6d6cc6a458585a5cd4855daaf2a48a377e41
zhangchunhui <boycot2017@163.com>
2025-03-12 18:57:28 +0800

    项目初始化

---
+++ application/extra/timming.php
@@ -0,0 +1,40 @@
+<?php
+return array (
+  'aa' => 
+  array (
+    'id' => 'aa',
+    'status' => '0',
+    'name' => 'aa',
+    'des' => '采集今日数据',
+    'file' => 'collect',
+    'param' => 'ac=cjday&xt=1&ct=&rday=24&cjflag=tv6_com&cjurl=http://cj2.tv6.com/mox/inc/youku.php',
+    'weeks' => '1,2,3,4,5,6,0',
+    'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
+    'runtime' => 1597629775,
+  ),
+  'bb' => 
+  array (
+    'status' => '0',
+    'name' => 'bb',
+    'des' => '生成首页',
+    'file' => 'make',
+    'param' => 'ac=index',
+    'weeks' => '1,2,3,4,5,6,0',
+    'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
+    'id' => 'bb',
+    'runtime' => 1535348998,
+  ),
+  '百度m3u8' => 
+  array (
+    'id' => '百度m3u8',
+    '__token__' => '450de946f355d7826a68224d75643e7e',
+    'status' => '1',
+    'name' => '百度m3u8',
+    'des' => '',
+    'file' => 'collect',
+    'param' => '/start.php/admin/collect/api.html?ac=cj&cjflag=6c4b9deb0102572741f5169070ce1b56&cjurl=https%3A%2F%2Fapi.apibdzy.com%2Fapi.php%2Fprovide%2Fvod%2F%3Fac%3Dlist&h=24&t=&ids=&wd=&type=2&mid=1&opt=0&sync_pic_opt=0&filter=0&filter_from=&filter_year=&param=',
+    'weeks' => '1,2,3,4,5,6,0',
+    'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
+    'runtime' => 1741772963,
+  ),
+);
