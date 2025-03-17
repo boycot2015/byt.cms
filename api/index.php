d777809730eed438c0fae5baf09235f293da9751
zhangchunhui <boycot2017@163.com>
2025-03-17 15:13:50 +0800

    222

--- api/index.php
+++ api/index.php
@@ -1,13 +1,13 @@
 <?php
-// $file= __DIR__ . '/..'.$_SERVER["PHP_SELF"];
+#$file= __DIR__ . '/..'.$_SERVER["PHP_SELF"];
 
-// if(file_exists($file))
-// {
-//    return false;
-// }
-// else
-// {
-//     require_once __DIR__ . '/../index.php';
-// }
+#if(file_exists($file))
+#{
+#   return false;
+#}
+#else
+#{
+#    require_once __DIR__ . '/../index.php';
+#}
 #echo $_SERVER["PHP_SELF"];
 phpinfo();
